d372fed9d6a299693becd7129faef360b7ed2125
aznamle <namle1998@hotmail.com>
2021-10-19 04:21:50 -0700

    changed cardloader color

--- src/components/CardLoader.js
+++ src/components/CardLoader.js
@@ -3,8 +3,8 @@ import React from 'react'
 const CardLoader = () => {
     return (
         <div className='animate-pulse space-y-2 py-4'>
-            <div className='h-72 w-5/6 bg-gray-400 rounded'></div>
-            <div className="h-4 bg-gray-400 rounded w-3/4"></div>
+            <div className='h-72 w-5/6 bg-gray-300 rounded'></div>
+            <div className="h-4 bg-gray-300 rounded w-3/4"></div>
         </div>
     )
 }
