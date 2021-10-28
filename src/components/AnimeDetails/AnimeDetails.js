94f31f8b25cc70eac1e2c9959eeff30ec9c6e5e8
aznamle <namle1998@hotmail.com>
2021-10-27 18:32:59 -0700

    styling

--- src/components/AnimeDetails/AnimeDetails.js
+++ src/components/AnimeDetails/AnimeDetails.js
@@ -43,28 +43,29 @@ const AnimeDetails = () => {
                 </Section>
 
                 <Section>
-                <div className='mx-auto max-w-screen-xl flex'>
-                <div className='py-4 space-y-4'>
-                    <div className='bg-white w-64 rounded-md flex items-center px-2 space-x-4'>
-                        <div className='py-2'>
-                        <AiFillStar fontSize='15px' className=' text-yellow-300'/>
+                <div className='mx-auto max-w-screen-xl space-x-8 flex'>
+                    <div className='py-4 space-y-4'>
+                        <div className='bg-white w-64 rounded-md flex items-center px-2 space-x-4'>
+                            <div className='py-2'>
+                            <AiFillStar fontSize='15px' className=' text-yellow-300'/>
+                            </div>
+                            <p className='text-sm font-semibold text-gray-500'>#{animeDetails.rank} Highest Ranked All Time</p>
                         </div>
-                        <p className='text-sm font-semibold text-gray-500'>#{animeDetails.rank} Highest Ranked All Time</p>
+
+                        <div className='bg-white w-64 rounded-md flex items-center px-2 space-x-4'>
+                            <div className='py-2'>
+                            <AiFillStar fontSize='15px' className=' text-yellow-300'/>
+                            </div>
+                            <p className='text-sm font-semibold text-gray-500'>#{animeDetails.popularity} Highest Rated</p>
+                        </div>
+
+                        <AnimeStats animeDetails={animeDetails} />
                     </div>
 
-                    <div className='bg-white w-64 rounded-md flex items-center px-2 space-x-4'>
-                        <div className='py-2'>
-                        <AiFillStar fontSize='15px' className=' text-yellow-300'/>
-                        </div>
-                        <p className='text-sm font-semibold text-gray-500'>#{animeDetails.popularity} Highest Rated</p>
+                    <div className='py-4 space-y-2 w-2/5'>
+                        <h1 className='text-md font-semibold text-gray-500'>Trailer</h1>
+                        <iframe className='rounded-md h-3/4 w-full' frameBorder='0' allowFullScreen='allowfullscreen' title='video' src={`${animeDetails.trailer_url}/?autoplay=false`} />
                     </div>
-
-                    <AnimeStats animeDetails={animeDetails} />
-                </div>
-
-                <div className='py-4 space-y-4 px-4 w-2/5'>
-                    <iframe className='rounded-md h-3/4 w-full' frameBorder='0' allowFullScreen='allowfullscreen' src={`${animeDetails.trailer_url}/?autoplay=false`} />
-                 </div>
 
                 </div>
                 </Section>
